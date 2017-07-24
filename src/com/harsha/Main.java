f4f4fa124c6ae8ad42c6174faaffbbce4b7086ae
rshravi <rshravi68@gmail.com>
2017-07-25 00:42:58 +0530

    Git practise1

---
+++ src/com/harsha/Main.java
@@ -0,0 +1,12 @@
+package com.harsha;
+
+import java.time.Clock;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("Hello Git Practise");
+
+    }
+}
