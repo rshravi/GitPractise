8635b969b9c81c78596f65f2e9dd00b94cecd2f8
Sriharsha <rshravi68@gmail.com>
2017-07-25 00:48:25 +0530

    Update Main.java

--- src/com/harsha/Main.java
+++ src/com/harsha/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.print("Hello Git Practise");
-	   System.out.println("added in git hub");
+	   System.out.println("added in git hub1");
 
     }
 }
